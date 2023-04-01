c010955a93c7e678c4140a5e8cdc8cc952fcfca0
Kotusyk <melnikanna524@gmail.com>
2023-04-01 21:45:19 +0300

    Added base for a project

---
+++ AnimalRecognizer/AnimalRecognizer/Model/Pet.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimalRecognizer.Model
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Breed { get; set; }
+        public string Colour { get; set; }
+
+    }
+}
