82826a6dc8ae04bd8418bb9426868b044960d836
Kotusyk <melnikanna524@gmail.com>
2023-05-07 01:01:33 +0300

    Fixed requests and added new table
    1. Fixed request problem in all controllers (For it I fix DBContext that for all string values allow only 1 character length )
    2. Recreated migration
    3. Added new Image table and One-To-Zero relationship with Pet

---
+++ AnimalRecognizer/AnimalRecognizer/Model/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimalRecognizer.Model
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Src { get; set; }
+        public string Alt { get; set; }
+        public Pet? Pet { get; set; }
+    }
+}
